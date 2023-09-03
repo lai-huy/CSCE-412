a214705bd78dd6b52676a85905129270354e4ae5
Huy Lai <lai.huy@tamu.edu>
2023-09-03 01:21:28 -0500

    update boot script

--- Project 1/boot_script.ps1
+++ Project 1/boot_script.ps1
@@ -1,7 +1,22 @@
-Invoke-WebRequest -Uri "https://download.oracle.com/java/17/latest/jdk-17_windows-x64_bin.msi" -OutFile "./java.exe"
-Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.42.0.windows.2/Git-2.42.0.2-64-bit.exe" -OutFile "./git.exe"
-Invoke-WebRequest -Uri "https://github.com/vim/vim-win32-installer/releases/download/v9.0.1839/gvim_9.0.1839_x64.exe" -Outfile "./vim.exe"
+<powershell>
+$loc = "c:\Users\Administrator\Desktop\debug.log"
 
-Start-Process -FilePath "./java.exe" -ArgumentList '/S' -Wait
-Start-Process -FilePath "./git.exe" -ArgumentList '/S' -Wait
-Start-Process -FilePath "./vim.exe" -ArgumentList '/S' -Wait
+Write-Output "Starting Script" >> $loc
+
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Write-Output "Installing VS Code" >> $loc
+choco install -y vscode --force --force-dependencies --verbose >> $loc
+
+Write-Output "Installing Git" >> $loc
+choco install -y git --force --force-dependencies --verbose >> $loc
+
+Write-Output "Installing Java" >> $loc
+choco install -y openjdk --force --force-dependencies --verbose >> $loc
+
+Write-Output "Install PDF Reader" >> $loc
+choco install -y adobereader --force --force-dependencies --verbose >> $loc
+
+Write-Output "Completed" >> $loc
+</powershell>
+<persist>false</persist>
