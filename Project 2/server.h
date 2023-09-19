3f9654bee05c44cca3dd9e12355a0c27205d9fbb
Huy Lai <lai.huy@tamu.edu>
2023-09-19 13:37:12 -0500

    add comments to server.h

--- Project 2/server.h
+++ Project 2/server.h
@@ -8,37 +8,82 @@
 using std::string, std::ostream;
 using std::cout;
 
+/**
+ * @brief The Server class represents a server that can handle requests.
+ */
 class Server {
 private:
-    string name;
-    Request current_request;
-    size_t current_wait;
+	/**
+	 * @brief Name of the server
+	 */
+	string name;
 
+	/**
+	 * @brief The current request being handled by the server.
+	 */
+	Request current_request;
+
+	/**
+	 * @brief The timestamp when the current request started to be handled.
+	 */
+	size_t current_wait;
 public:
-    Server(const string& name) : name{name}, current_request{Request{}}, current_wait{0} {}
+	/**
+	 * @brief Construct a new Server object
+	 *
+	 * @param name The name of the server
+	 */
+	Server(const string& name) : name{name}, current_request{Request{}}, current_wait{0} {}
 
-    void handle_request(const Request& request, size_t timestamp) {
-        this->current_request = request;
-        this->current_wait = timestamp;
-        cout << *this << " is handling Request:\t" << this->current_request << "\n";
-    }
+	/**
+	 * @brief Handles a request by the server.
+	 *
+	 * @param request The request to be handled.
+	 * @param timestamp The timestamp when the request is received.
+	 */
+	void handle_request(const Request& request, size_t timestamp) {
+		this->current_request = request;
+		this->current_wait = timestamp;
+		cout << *this << " is handling Request:\t" << this->current_request << "\n";
+	}
 
-    friend ostream& operator<<(ostream& os, const Server& server) {
-        os << "Server{name=" << server.name << "}";
-        return os;
-    }
+	/**
+	 * @brief Friend function for outputting server information to an ostream.
+	 *
+	 * @param os The output stream.
+	 * @param server The server to be output.
+	 * @return ostream& The output stream after writing server information.
+	 */
+	friend ostream& operator<<(ostream& os, const Server& server) {
+		os << "Server{name=" << server.name << "}";
+		return os;
+	}
 
-    const Request& getRequest() const {
-        return this->current_request;
-    }
+	/**
+	 * @brief Gets the current request being handled by the server.
+	 * @return const Request& A constant reference to the current request.
+	 */
+	const Request& getRequest() const {
+		return this->current_request;
+	}
 
-    const string& getName() const {
-        return this->name;
-    }
+	/**
+	 * @brief Get the Name object
+	 *
+	 * @return const string& A constant reference to the name of the server.
+	 */
+	const string& getName() const {
+		return this->name;
+	}
 
-    const size_t& time() const {
-        return this->current_wait;
-    }
+	/**
+	 * @brief Gets the timestamp when the current request started to be handled.
+	 *
+	 * @return const size_t& A constant reference to the timestamp.
+	 */
+	const size_t& time() const {
+		return this->current_wait;
+	}
 };
 
 #endif // SERVER_H
